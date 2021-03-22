55cd173ec8dc74e9f9d342f85f64a3d52858cc15
JCabrol <justine.cabrol14@gmail.com>
2021-03-22 19:26:17 +0100

    Correct function calculateFare
    -Change data format from int to double
    -Change used function getHours() to getTime()
    
     to be able to calculate durations
    
     -bigger than 24 hours
     -smaller than one hour

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,6 +3,9 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class FareCalculatorService {
 
     public void calculateFare(Ticket ticket){
@@ -10,18 +13,25 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        // Calculate time the vehicle stays in the parking. The result is in milliseconds.
+        long inMilliseconds = ticket.getInTime().getTime();
+        long outMilliseconds = ticket.getOutTime().getTime();
+        long durationInMilliseconds = outMilliseconds - inMilliseconds;
+        //Convert the duration in hours. The result is not rounded.
+        double durationWithTooMuchNumbers = (double)durationInMilliseconds/3600000;
+        //Round the result to have only two numbers after the dot.
+        BigDecimal bd = new BigDecimal(durationWithTooMuchNumbers).setScale(2, RoundingMode.HALF_UP);
+        double duration = bd.doubleValue();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
+                //Calculate price if the vehicle is a car.
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
                 break;
             }
             case BIKE: {
+                //Calculate price if the vehicle is a bike.
                 ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
